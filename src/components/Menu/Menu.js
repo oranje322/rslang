0d19835c90f1def063af5bb765c5f109c6495822
zaruba2004 <zaruba2004gmail.com>
2021-03-26 23:25:28 +0300

    correct left menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -5,36 +5,36 @@ import list from '@assets/img/list.png';
 import game from '@assets/img/game.png';
 import stat from '@assets/img/stat.png';
 import settings from '@assets/img/settings.png';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const Menu = () => {
 	return (
 		<div className={styles.menu}>
-			<Link to={'/book'} activeclassname={styles.item_active}>
+			<NavLink to={'/book'} exact activeClassName={styles.item_active}>
 				<div className={styles.item}>
 					<img className={styles} src={book} alt="book" />
 				</div>
-			</Link>
-			<Link to={'/learn'}>
+			</NavLink>
+			<NavLink to={'/learn'} activeClassName={styles.item_active}>
 				<div className={styles.item}>
 					<img src={list} alt="list" />
 				</div>
-			</Link>
-			<Link to={'/games'}>
+			</NavLink>
+			<NavLink to={'/games'} activeClassName={styles.item_active}>
 				<div className={styles.item}>
 					<img src={game} alt="game" />
 				</div>
-			</Link>
-			<Link to={'/stats'}>
+			</NavLink>
+			<NavLink to={'/stats'} activeClassName={styles.item_active}>
 				<div className={styles.item}>
 					<img src={stat} alt="stat" />
 				</div>
-			</Link>
-			<Link to={'/settings'}>
+			</NavLink>
+			<NavLink to={'/settings'} activeClassName={styles.item_active}>
 				<div className={styles.item}>
 					<img src={settings} alt="settings" />
 				</div>
-			</Link>
+			</NavLink>
 		</div>
 	);
 };
