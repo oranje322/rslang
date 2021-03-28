1fa733e6a5f307b26ec2b3829fab4e09f07fe779
oranje <oranje322@mail.ru>
2021-03-29 02:58:39 +0300

    fix menu visible

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles/app.global.scss'; // global styles
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Main from './containers/Main/Main';
-import { Provider } from 'react-redux';
-import store from './redux/store';
 import Book from './containers/Book/Book';
 import Learn from './containers/Learn/Learn';
 import Games from './containers/Games/Games';
@@ -12,36 +10,48 @@ import Settings from './containers/Settings/Settings';
 import Words from './containers/Words/Words';
 import Menu from './components/Menu/Menu';
 import Footer from './components/Footer/Footer';
+import { useLocation } from 'react-router';
 
 const App = () => {
+	const [menuVisible, setMenuVisible] = useState(false);
+	const location = useLocation();
+
+	useEffect(() => {
+		if (location.pathname === '/') {
+			setMenuVisible(false);
+		} else {
+			setMenuVisible(true);
+		}
+	}, [location, menuVisible]);
+
 	return (
-		<Provider store={store}>
-			<BrowserRouter>
-				<Switch>
-					<Route path={'/book'}>
-						<Book />
-					</Route>
-					<Route path={'/learn'}>
-						<Learn />
-					</Route>
-					<Route path={'/games'}>
-						<Games />
-					</Route>
-					<Route path={'/stats'}>
-						<Stats />
-					</Route>
-					<Route path={'/settings'}>
-						<Settings />
-					</Route>
-					<Route path={'/words'}>
-						<Words />
-					</Route>
-					<Route exact path="/" component={Main} />
-				</Switch>
-				<Menu />
-				{window.location.pathname !== '/game' ? <Footer /> : null}
-			</BrowserRouter>
-		</Provider>
+		<>
+			<Switch>
+				<Route path={'/book'}>
+					<Book/>
+				</Route>
+				<Route path={'/learn'}>
+					<Learn/>
+				</Route>
+				<Route path={'/games'}>
+					<Games/>
+				</Route>
+				<Route path={'/stats'}>
+					<Stats/>
+				</Route>
+				<Route path={'/settings'}>
+					<Settings/>
+				</Route>
+				<Route path={'/words'}>
+					<Words/>
+				</Route>
+				<Route exact path="/" component={Main}/>
+			</Switch>
+			{
+				menuVisible && <Menu/>
+			}
+			{location.pathname !== '/games' ? <Footer/> : null}
+		</>
 	);
 };
 
