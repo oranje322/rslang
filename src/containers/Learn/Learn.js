2cf74d51610b59e650f9ab501a077d5e84046c57
Stefaniya Yanovich <stefanishy@gmail.com>
2021-03-30 19:03:10 +0300

    fix: css buttons,footer

--- src/containers/Learn/Learn.js
+++ src/containers/Learn/Learn.js
@@ -8,31 +8,31 @@ import { Link } from 'react-router-dom';
 const Learn = () => {
 	return (
 		<div>
-			<Header title={'Изучение'}/>
+			<Header title={'Изучение'} />
 			<div className={style.learnBox}>
 				<h2>Здесь Вы можете выбрать категорию слов для изучения и просмотреть слова, которые Вы решили удалить.</h2>
 				<div className={style.learnMain}>
 					<div className={style.learnCard}>
 						<h3>Активные слова</h3>
 						<Link to={'/learn/active'}>
-							<button>Проверить</button>
+							<button className={style.button}>Проверить</button>
 						</Link>
 					</div>
 					<div className={style.learnCard}>
 						<h3>Сложные слова</h3>
 						<Link to={'/learn/hard'}>
-							<button>Проверить</button>
+							<button className={style.button}>Проверить</button>
 						</Link>
 					</div>
 					<div className={style.learnCard}>
 						<h3>Удаленные слова</h3>
 						<Link to={'/learn/delete'}>
-							<button>Проверить</button>
+							<button className={style.buttonDel}>Проверить</button>
 						</Link>
 					</div>
 				</div>
 			</div>
-			<FooterBackground/>
+			<FooterBackground />
 		</div>
 	);
 };
