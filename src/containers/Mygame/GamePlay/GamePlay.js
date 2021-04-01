bbb1a12c95cfe5a991341af4e0adafe3c6c136ef
Stefaniya Yanovich <stefanishy@gmail.com>
2021-04-01 23:04:46 +0300

    add: basic my-game logic

--- src/containers/Mygame/GamePlay/GamePlay.js
+++ src/containers/Mygame/GamePlay/GamePlay.js
@@ -1,24 +1,64 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-const GamePlay = () => {
+import classes from './GamePlay.module.scss';
+// import Recognition from './Recognition'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 
-    const words = useSelector(state => state.words);
-    let someWords = words ? words.activeWords.slice(0, 5) : null;
+const GamePlay = ({ comingWords }) => {
 
-    useEffect(() => {
-        console.log('words: ', words);
-    }, []);
+    let words = ['cat', 'beautiful', 'advertise', 'game', 'digital']
+    let someWord = words[Math.floor(Math.random() * words.length)];
+
+    const [newWord, setNewWord] = useState(someWord);
+
+    const { transcript, resetTranscript, listening } = useSpeechRecognition();
+
+    if (!SpeechRecognition.browserSupportsSpeechRecognition()) {
+        return <h2>Распознавание речи не поддерживается в Вашем бразуере :(</h2>
+    }
+
+    const startRecognition = () => {
+        SpeechRecognition.startListening();
+    }
+
+    // useEffect(() => {
+    //     chooseWord();
+    // }, [transcript])
 
     return (
-        <div>
-            {someWords.map(word => (
-                <div key={word._id}>
-                    <h3>{word.word}</h3>
-                </div>
-            ))}
-        </div>
+        <div className={classes.cardContainer}>
+            {
+                <>
+                    <h3>{newWord}</h3>
+                </>
+            }
+            <div className={classes.recognitionBlock}>
+                {!transcript && <button onClick={startRecognition}>Начать</button>}
+                {transcript ?
+                    <>
+                        <p>{transcript}</p>
+                        {transcript === newWord ?
+                            <>
+                                <p>Правильно!</p>
+                                <button onClick={() => {
+                                    resetTranscript();
+                                    setNewWord(someWord);
+                                }}>Новое слово</button>
+                            </>
+                            :
+                            <>
+                                <p>Что-то не так:(</p>
+                                <button onClick={() => {
+                                    resetTranscript();
+                                    startRecognition();
+                                }}>Еще раз</button>
+                            </>
+                        }
+                    </> : listening ? <p>Cлушаю</p> : null}
 
-    );
+            </div>
+        </div>
+    )
 };
 
 export default GamePlay;
