df21e4a292bed8260ae3fb1e2a7706af2b598024
Stefaniya Yanovich <stefanishy@gmail.com>
2021-04-03 13:06:24 +0300

    edit:mygame

--- src/containers/Mygame/GamePlay/GamePlay.js
+++ src/containers/Mygame/GamePlay/GamePlay.js
@@ -1,14 +1,36 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import classes from './Mygame.module.scss';
+
 const GamePlay = () => {
     const words = useSelector(state => state.words);
-    console.log(words);
+    const { finalTranscript, resetTranscript, listening } = useSpeechRecognition()
+    const [lifes, setLifes] = useState(5);
+    const [isGamePlayed, setIsGamePlayed] = useState(false);
+    const [wordsPosition, setWordsPosition] = useState('70%');
+    const [allWords, setAllWords] = useState();
+    const [levelWords, setLevelWords] = useState();
+    const [correctWord, setCorrectWord] = useState();
+    const [statistics, setStatistics] = useState(0);
+    const winStats = 10;
+
+    useEffect(async () => {
+        const res = await getAllAggregatedWords(0, 0, 34, '{"$or":[{"userWord.difficulty":"hard"},{"userWord":null}]}');
+        const resWords = res[0].paginatedResults;
+        setAllWords(resWords);
+        console.log('resWords: ', resWords);
+    }, []);
+
     return (
 
-        <h2>начали</h2>
+        <div className={classes.gameContainer}>
+
+        </div>
 
 
     );
 };
 
 export default GamePlay;
+
--- src/redux/actions/WordsActions.js
+++ src/redux/actions/WordsActions.js
@@ -1,8 +1,3 @@
-import { DELETE_WORD, SET_DIFFICULT, SET_GROUP, SET_PAGE, SET_TOTAL_COUNT, SET_WORDS } from '../reducers/wordsReducer';
+import { SET_WORDS } from '../reducers/wordsReducer';
 
 export const setWordsAC = (payload) => ({type: SET_WORDS, payload})
-export const setDifficulty = (payload) => ({type: SET_DIFFICULT, payload})
-export const setPage = (payload) => ({type: SET_PAGE, payload})
-export const setGroup = (payload) => ({type: SET_GROUP, payload})
-export const deleteWordAC = (payload) => ({type: DELETE_WORD, payload})
-export const setTotalCount = (payload) => ({type: SET_TOTAL_COUNT, payload})
