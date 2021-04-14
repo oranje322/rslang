55a772e69d924007c09f8211505354f0d6336d96
zaruba2004 <zaruba2004gmail.com>
2021-04-14 09:27:46 +0300

    add words widowithout authorization

--- src/containers/Audiocall/Audiocall.js
+++ src/containers/Audiocall/Audiocall.js
@@ -4,26 +4,20 @@ import Header from '../../components/Header/Header';
 import Menu from '../../components/Menu/Menu';
 import styles from './Audiocall.module.scss';
 import GameField from './GameField';
-import { getAllAggregatedWords } from '../../api/api';
+// import { getAllAggregatedWords } from '../../api/api';
 import { loadWordsForGame } from '../../redux/thunk/gamesThunk';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Audiocall = () => {
 	const [startGame, setStartGame] = useState(false);
-    const [allWords, setAllWords] = useState();
 	const [randomNumArr, setRandomNumArr] = useState();
 	const dispatch = useDispatch();
     const { words } = useSelector(state => state.games);
+	console.log(words)
 
     useEffect(() => {
         dispatch(loadWordsForGame());
-    }, []);
-
-	useEffect(async () => {
-        const res = await getAllAggregatedWords(0, 0, 34, '{"$or":[{"userWord.difficulty":"hard"},{"userWord":null}]}');
-        const resWords = res[0].paginatedResults;
-        setAllWords(resWords);
-		randomNumWords();	
+		randomNumWords();
     }, []);
 
 	const returnToStart = () => {
@@ -45,15 +39,15 @@ const Audiocall = () => {
 			<Header title={'Аудио вызов'}/>
 			<Menu />
 			{startGame
-			? <GameField returnToStart={returnToStart} words={allWords} randomNumArr={randomNumArr}/>	
+			? <GameField returnToStart={returnToStart} words={words} randomNumArr={randomNumArr}/>	
 			: (	<div className={styles.rulesField}>
 				<div>
 					<h1>АУДИОВЫЗОВ</h1>
 					<p>Мини-игра «Аудиовызов» - это тренировка, развивающая навыки речи и перевода.</p>
 					<p> Вы слышите слово и видите 10 вариантов перевода. Нужно выбрать правильный ответ кликнув по нему мышью.</p> 
-					{ allWords == undefined
-					? <p>Авторизуйтесь!!!</p>
-					: <button className={styles.startGameButton} onClick={()=>setStartGame(true)}>Начать</button>}						
+					{ words == false
+					? <p>У вас нет активных слов, перейдите в раздел Учебник и выберите раздел</p>
+					: <button className={styles.startGameButton} onClick={()=>setStartGame(true)}>Начать</button>}
 					<NavLink style={{ textDecoration: 'none' }} to={'/games'}>
 						<button className={styles.closeGame}>&#9664;</button>
 					</NavLink>
